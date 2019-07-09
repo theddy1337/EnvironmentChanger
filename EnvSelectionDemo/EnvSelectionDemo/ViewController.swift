50b60d09e7c6444bc4ca805e6681e38eb69e1fe3
Gavril Tonev <gavril.tonev@upnetix.com>
2019-07-09 12:45:40 +0300

    Added Demo project

---
+++ EnvSelectionDemo/EnvSelectionDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  EnvSelectionDemo
+//
+//  Created by Gavril Tonev on 9.07.19.
+//  Copyright © 2019 Gavril Tonev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
